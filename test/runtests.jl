e865f69acdf9ea82a9c2f9ac5129e1eed203d4f8
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2024-12-23 18:27:31 +0100

    Organizing deps

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,3 @@
 # -*- coding: utf-8 -*-
 using Test
 using AuChimiste
-using ChemicalElements
-using ChemicalComponents
-using ChemicalKinetics
-using ChemicalReactors
-using CombustionChemistry
-using PhysicalChemistry
