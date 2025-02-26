54749723249553a424e2ee17f2afc743806a5502
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2025-02-26 09:35:31 +0100

    WIP

--- src/database.jl
+++ src/database.jl
@@ -4,6 +4,10 @@ export load_path
 export add_load_path
 export reset_load_path
 
+#######################################################################
+# CONSTANTS
+#######################################################################
+
 "Default search path for thermodynamics and kinetics databases."
 const DATA_PATH = joinpath(dirname(@__DIR__), "data")
 
@@ -12,7 +16,27 @@ const USER_PATH = [DATA_PATH, pwd(), expanduser("~")]
 
 # XXX: until I get to understand artifacts...
 # const THERMO_COMPOUND_DATA = artifact"thermo_compounds"
-const THERMO_COMPOUND_DATA = joinpath(DATA_PATH, "thermo_compounds.yaml")
+const THERMO_COMPOUND_DATA = "thermo_compounds.yaml"
+
+#######################################################################
+# WARNINGS
+#######################################################################
+
+THERMO_WARNINGS = true
+
+function disable_thermo_warnings()
+    global THERMO_WARNINGS = false
+    return nothing
+end
+
+function enable_thermo_warnings()
+    global THERMO_WARNINGS = true
+    return nothing
+end
+
+#######################################################################
+# PATH MANAGEMENT
+#######################################################################
 
 function load_path()
     return sort(deepcopy(USER_PATH))
@@ -34,10 +58,14 @@ end
 # INTERNALS
 #######################################################################
 
-function get_data_file(name)
+function get_data_file(name; which = false)
     for path in USER_PATH
         tentative = joinpath(path, name)
-        isfile(tentative) && return tentative
+
+        if isfile(tentative)
+            which && @info("Data file `$(name)` found at `$(path)`")
+            return tentative
+        end
     end
 
     path =  join(map(n->"- $(n)", USER_PATH), "\n")
