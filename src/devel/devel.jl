9c15e6f097116e769104773d7145130b76c9929a
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2025-04-22 07:44:46 +0200

    WIP

--- src/devel/devel.jl
+++ src/devel/devel.jl
@@ -2,13 +2,33 @@ using AuChimiste
 
 db = let
     data_file = "nasa_gas.yaml"
-	selected_species = ["CH4", "O2", "CO2", "H2O"]
+	selected_species = ["C", "H", "O", "CH4", "O2", "CO2", "H2O"]
 	AuChimisteDatabase(; data_file, selected_species)
 end;
 
 species = db.species;
 
-h(T) = species.CH4.thermo.func.enthalpy(T);
+# h_ref = species.CH4.thermo.data.h_ref;
 
-h_ref = species.CH4.thermo.data.h_ref;
-h0 = h(298.15)
+# h0 = let
+#     h = species.CH4.thermo.func.enthalpy(298.15)
+#     h -= 1species.H.thermo.func.enthalpy(298.15)
+#     h -= 4species.C.thermo.func.enthalpy(298.15)
+#     h
+# end
+
+# https://webbook.nist.gov/cgi/cbook.cgi?ID=C74828&Mask=1
+species.CH4.thermo.func.enthalpy(298.15)
+
+# https://webbook.nist.gov/cgi/cbook.cgi?ID=C124389&Mask=1
+species.CO2.thermo.func.enthalpy(298.15)
+
+# https://webbook.nist.gov/cgi/cbook.cgi?ID=C7732185&Mask=1#Thermo-Gas
+species.H2O.thermo.func.enthalpy(298.15)
+
+let h = 0
+    h += 1species.CH4.thermo.func.enthalpy(298.15)
+    h += 2species.O2.thermo.func.enthalpy(298.15)
+    h -= 1species.CO2.thermo.func.enthalpy(298.15)
+    h -= 2species.H2O.thermo.func.enthalpy(298.15)
+end
